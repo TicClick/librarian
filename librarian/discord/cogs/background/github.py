3515814a3120b5e0552073159d78448b4029fcb6
TicClick <ya@ticclick.ch>
2021-04-29 22:18:32 +0200

    eliminate race condition between two routines that fetch pulls

--- librarian/discord/cogs/background/github.py
+++ librarian/discord/cogs/background/github.py
@@ -31,7 +31,7 @@ class FetchNewPulls(base.BackgroundCog):
 
     LAST_PULL = "last_pull"
     SHORT_INTERVAL = 3
-    LONG_INTERVAL = 60
+    LONG_INTERVAL = 600
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -58,6 +58,19 @@ class FetchNewPulls(base.BackgroundCog):
             pull_data = await self.github.get_single_pull(self.last_pull)
             if pull_data is not None:
                 logger.info("%s: fetched pull #%s", self.name, self.last_pull)
+                if pull_data["state"] == formatters.PullState.OPEN.name:
+                    number = pull_data["number"]
+                    if self.storage.pulls.by_number(number):
+                        logger.info("%s: skipping open pull #%d (fetched already)", self.name, number)
+                        self.last_pull += 1
+                    else:
+                        logger.info(
+                            "%s: open pull #%d is not fetched by %s. going into slow mode",
+                            self.name, number, MonitorPulls.name
+                        )
+                        self.loop.change_interval(seconds=self.LONG_INTERVAL)
+                    return
+
                 try:
                     self.storage.pulls.save_from_payload(pull_data, insert=True)
                 except sql_exc.IntegrityError:  # fetched by MonitorPulls
